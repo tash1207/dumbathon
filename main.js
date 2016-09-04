008f89efa066709d196ec5febe86c8a9440a0f7f
Tasha Hankewych <tashawych@tashawych-macbookpro.roam.corp.google.com>
2016-09-04 13:50:13 -0700

    Implement basic version of the generator from a short list of companies and nouns.

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-var projectTitle = document.querySelector('h1');
-projectTitle.textContent = 'Pandora for cats';
+function capitalize(word) {
+  word = word || this;
+  return word.charAt(0).toUpperCase() + word.substr(1);
+}
 
+String.prototype.capitalize = capitalize;
+
+function generateProject() {
+
+  var projectTitle = document.querySelector('h1');
+
+  var companies = ['Facebook', 'Github', 'Google', 'Pandora', 'Tinder', 'Twitch', 'Uber']
+  var nouns = ['babies', 'cars', 'cats', 'dogs', 'food', 'gnar', 'music', 'skateboarding']
+
+  var getRandom = function(list) {
+    return list[Math.floor((Math.random()*list.length))];
+  };
+
+  var company = getRandom(companies);
+  var noun = getRandom(nouns);
+
+  projectTitle.textContent = `${company} for ${noun.capitalize()}`;
+}
+
+generateProject();
